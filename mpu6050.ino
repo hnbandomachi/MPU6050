5841b7bd632ad6518159792692999371654011cd
Khiem Hai <hnbandomachi@gmail.com>
2019-11-20 11:14:00 +0700

    update angle

--- mpu6050.ino
+++ mpu6050.ino
@@ -2,7 +2,8 @@
 #include "mpu6050.h"
 
 LiquidCrystal lcd(8, 7, 5, 4, 3, 2);
-int angleX, angleY, angleZ;
+float x, y, z;
+float angle;
 void setup() {
   // put your setup code here, to run once:
    lcd.begin(16, 2);
@@ -12,25 +13,35 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  angleX = map(get_accx(), -16384, 16384, -90, 90);
-  angleY = map(get_accy(), -16384, 16384, -90, 90);
-  angleZ = map(get_accz(), -16384, 16384, -90, 90);
+  x = map(get_accx(), -16384, 16384, -90, 90);
+  y = map(get_accy(), -16384, 16384, -90, 90);
+  z = map(get_accz(), -16384, 16384, -90, 90);
 
-  Serial.print(" angleX: ");
-  Serial.print(angleX);
-  Serial.print(" angleY: ");
-  Serial.print(angleY);
-  Serial.print(" angleZ: ");
-  Serial.println(angleZ);
-  
-  if(angleY > 90 || angleY < 0) {
-    angleY = abs(angleX);
-    if(angleY > 90) angleY = 180 - angleX;
-  }     
+  x = x*3.141592/180;
+  y = y*3.141592/180;
+  z = z*3.141592/180;
+
+  angle = acos(sin(z)/sqrt(sin(x)*sin(x) + sin(y)*sin(y) + sin(z)*sin(z)));
+  angle = angle/3.141592*180;
+
+//  if(x > y) angle = x;
+//  else      angle = y;
+//
+//  if(angle > 90) angle = 180 - angle;
+
+  Serial.print(" X: ");
+  Serial.print(x);
+  Serial.print(" Y: ");
+  Serial.print(y);
+  Serial.print(" Z: ");
+  Serial.print(z);
+  Serial.print(" angle: ");
+  Serial.println(angle);
+       
   lcd.setCursor(0, 0);    
   lcd.print("angle: ");
   lcd.setCursor(7, 0);
-  lcd.print(angleY);  
+  lcd.print(angle);  
   delay(1000);
   lcd.clear();
 }
